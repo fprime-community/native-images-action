c2f8d9d41df3ba94cbba2784c8853bbe2406aeff
Michael D Starch <Michael.D.Starch@jpl.nasa.gov>
2023-08-01 17:18:30 -0700

    Adding error message

--- src/fprime_native_images/jar.py
+++ src/fprime_native_images/jar.py
@@ -1,5 +1,6 @@
 import subprocess
 import sys
+import shutil
 
 
 def run():
@@ -8,6 +9,9 @@ def run():
     Runs a JAR file forwarding in all the arguments to the JAR file itself. The JAR file chosen is inferred from the
     name of the python executable (i.e. sys.arv[0]). Exits forwarding back the return code.
     """
+    if not shutil.which("java"):
+        print(f"[ERROR] {sys.argv[0]} requires 'java'. Please install 'java' and ensure it is available on the PATH.")
+        sys.exit(-23)
     arguments = ["java", "-jar", f"{sys.argv[0]}.jar"] + sys.argv[1:]
     process = subprocess.run(arguments)
     sys.exit(process.returncode)
